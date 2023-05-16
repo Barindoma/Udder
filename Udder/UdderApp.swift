2171861ca57356ad89e959e8bea687cb457873ee
Barindoma <barindoma@gmail.com>
2023-05-16 21:22:48 +0300

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by a on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
